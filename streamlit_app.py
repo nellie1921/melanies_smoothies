6540880090c491b617e6de360b481eb6d509ea79
nellie1921 <lisacuscuna@yahoo.com>
2026-07-02 13:03:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,16 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# Snowflake connection
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Load fruit list including SEARCH_ON column
 my_dataframe = (
     session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
     .select(col("FRUIT_NAME"), col("SEARCH_ON"))
 )
 
-# Convert to Pandas
 pd_df = my_dataframe.to_pandas()
 
-# Create list of fruit names for the multiselect
 fruit_options = pd_df["FRUIT_NAME"].tolist()
 
 ingredients_list = st.multiselect(
@@ -40,24 +36,15 @@ if ingredients_list:
 
         ingredients_string += fruit_chosen + " "
 
-        # Find SEARCH_ON value for this fruit
         search_on = pd_df.loc[
             pd_df["FRUIT_NAME"] == fruit_chosen,
             "SEARCH_ON"
         ].iloc[0]
 
-        st.write(
-            "The search value for",
-            fruit_chosen,
-            "is",
-            search_on,
-            "."
-        )
-
         st.subheader(fruit_chosen + " Nutrition Information")
 
         smoothiefruit_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         )
 
         st.dataframe(
